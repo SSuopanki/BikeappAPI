93ac52ec31b432f90b4c6e732215a98e825505c2
SSuopanki <koulujutut.sami@gmail.com>
2023-05-12 19:51:33 +0300

    Add Cors policy

--- Program.cs
+++ Program.cs
@@ -3,6 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var AllowOrigins = "AllowOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://127.0.0.1:5173");
+                      });
+});
 // Add services to the container.
 
 builder.Services.AddDbContext<BikeappContext>(options =>
@@ -14,12 +24,15 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(AllowOrigins);
 
 app.UseHttpsRedirection();
 
